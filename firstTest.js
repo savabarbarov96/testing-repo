22b11fac582f6ccceec22621b326cdd5989f2c0b
savabarbarov96 <45712571+savabarbarov96@users.noreply.github.com>
2022-09-12 18:43:29 +0300

    Revert "Code updated to run on lambdatest cloud."

--- firstTest.js
+++ firstTest.js
@@ -4,81 +4,50 @@ const { Builder, By } = require("selenium-webdriver");
 require("chromedriver");
 require("geckodriver");
 
-const ltCapabilities = require("../testing-repo/capabilities"); //lambdatest
-
 var should = require("chai").should();
 
-var assert = require("assert");
+var assert = require('assert');
 
 
+describe("First Test", function(){
+
+  it("Test success",async function(){
 
 
-// url
-const urlEndPoint = "https://strategyinrush.com/"
+      // Start the browser
+      let driver = await new Builder().forBrowser("chrome").build();
+    
+      // set window size
+      await driver.manage().window().setRect({ width: 1900, height: 1400 });
+      // Get the URL
+      await driver.get("https://strategyinrush.com/");
+    
+      // Click news BY ID
+      // await driver.findElement(By.id("menu-item-73864")).click()
+    
+      // By CSS
+      // await driver.findElement(By.css("#menu-item-73864 > a")).click()
+    
+      await driver.findElement(By.css("#menu-item-73864 > a")).click();
+    
+      // Let checkNewsText assertion to variable
+      let checkNewsText = await driver
+        .findElement(By.css("h1"))
+        .getText()
+        .then(function (value) {
+          return value;
+        });
+    
+    
+        // Chai should assertion
+      checkNewsText.should.equal("NEWS");
+    console.log("Test is " + checkNewsText)
+    
+      //Quit the session
+      driver.close();
+    
+  })
 
-//lambdatest
-describe("First Test", function () {
-
-  var driver; //lambdatest
-
-
-  //username
-  const USERNAME = "savabarbarov96";
-  //key
-  const KEY = "43rlSblNKFQrmjFXxjwj6odqbteFTSrAGjQEqel36LLhTpJroQ";
-
-  //host
-  const GRID_HOST = "hub.lambdatest.com/wd/hub";
-
-  const gridUrl = "https://" + USERNAME + ':' + KEY + '@' + GRID_HOST;
-
-  beforeEach(function () {
-    driver = new Builder()
-      .usingServer(gridUrl)
-      .withCapabilities(ltCapabilities.capabilities)
-      .build();
-  });
-
-  // afterEach(async function () {
-  //   await driver.quit();
-  // });
-  // //lambdatest
-
-  it("First test", async function () {
-    // Start the browser
-    // let driver = await new Builder().forBrowser("chrome").build();
-
-    // Get the URL
-    await driver.get(urlEndPoint)
-    // set window size
-    await driver.manage().window().setRect({ width: 1900, height: 1400 });
+})
 
 
-
-    // Click news BY ID
-    // await driver.findElement(By.id("menu-item-73864")).click()
-
-    // By CSS
-    // await driver.findElement(By.css("#menu-item-73864 > a")).click()
-
-    await driver.findElement(By.css("#menu-item-73864 > a")).click();
-
-    // Let checkNewsText assertion to variable
-    let checkNewsText = await driver
-      .findElement(By.css("h1"))
-      .getText()
-      .then(function (value) {
-        return value;
-      });
-
-      console.log(checkNewsText)
-    // Chai should assertion
-    checkNewsText.should.equal("NEWS");
-    console.log("Test is " + checkNewsText);
-
-    //Quit the session
-    driver.close();
-  });
-
-  
-});
